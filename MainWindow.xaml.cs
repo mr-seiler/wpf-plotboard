46ef31e9fdae9d64aed3a17c7c7266393fa374d0
Matthew Seiler <theflyinghyrax@gmail.com>
2015-01-14 16:23:31 -0500

    Drag'n'drop first pass

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 
 namespace PlottingBoard
 {
+    public enum MarkColors { RED, GREEN, BLUE };
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,8 +25,8 @@ namespace PlottingBoard
         public MainWindow()
         {
             InitializeComponent();
-            drawGridLines(GridCanvas, Brushes.LightGreen, 120, true, 10);
 
+            drawGridLines(GridCanvas, Brushes.LightGreen, 120, true, 10);
         }
 
         private void drawGridLines(Canvas canvas, Brush brush, int numLines, bool indexLines, int indexInterval)
@@ -78,6 +80,107 @@ namespace PlottingBoard
                 canvas.Children.Add(l);
             }
 
+        }
+
+        /* seperate mousemove handlers, so we can tell which button is which without hacks or a custom compund control (extending button) */
+
+        private void RedMarkBtn_MouseMove(object sender, MouseEventArgs e)
+        {
+            MarkBtn_MouseMove(sender, e, MarkColors.RED);
+        }
+
+        private void GreenMarkBtn_MouseMove(object sender, MouseEventArgs e)
+        {
+            MarkBtn_MouseMove(sender, e, MarkColors.GREEN);
+        }
+
+        private void BlueMarkBtn_MouseMove(object sender, MouseEventArgs e)
+        {
+            MarkBtn_MouseMove(sender, e, MarkColors.BLUE);
+        }
+
+        private void MarkBtn_MouseMove(object sender, MouseEventArgs e, PlottingBoard.MarkColors color)
+        {
+            WrapPanel btn = sender as WrapPanel;
+
+            Console.WriteLine("In MouseMove, " + BlueMarkBtn.ToString() + " " + e.LeftButton);
+
+            if (btn != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                Console.WriteLine("DODRAGDROP");
+
+                // pack up the color we want to use...
+                DataObject dragData = new DataObject("markColor", color);
+                // initialize the boogey
+                DragDrop.DoDragDrop(btn, dragData, DragDropEffects.Copy);
+            }
+
+        }
+
+        private void MarkerArea_DragEnter(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("DragEnter");
+            
+            // if the drag data isn't a marker color, then stop the drop
+            if (!e.Data.GetDataPresent("markColor"))
+            {
+                Console.WriteLine("Setting effect to none");
+                e.Effects = DragDropEffects.None;
+            }
+            // can also check source to stop drag/drop onto self:
+            // if (!e.Data.GetDataPresent("markColor") || sender == e.Source)
+        }
+
+        private void MarkerArea_Drop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("In Drop");
+
+            // casting to an enum.  delicious!
+            MarkColors color = (MarkColors)e.Data.GetData("markColor");
+
+            // get position of drop relative to marker area (rectangle)
+            Point where = e.GetPosition(MarkerArea);
+
+            // we know the color (sort of), and we know where to put it.
+            // create a new ellipse (how?)
+            // add it to a dictionary, so we can keep track of it.
+            // add it as a child of the rectangle.  maybe.
+            Ellipse m = buildMarker(color);
+            markers.Add(m);
+
+            MarkerArea.Children.Add(m);
+            Canvas.SetTop(m, where.Y - (m.Height / 2));
+            Canvas.SetLeft(m, where.X - (m.Width / 2));
+        }
+
+        private List<Ellipse> markers = new List<Ellipse>();
+
+        private static Ellipse buildMarker(MarkColors color)
+        {
+            Ellipse mark = new Ellipse();
+
+            mark.Width = 20;
+            mark.Height = 20;
+
+            switch (color)
+            {
+                case MarkColors.BLUE:
+                    mark.Fill = Brushes.Blue;
+                    break;
+                case MarkColors.GREEN:
+                    mark.Fill = Brushes.Green;
+                    break;
+                case MarkColors.RED:
+                    mark.Fill = Brushes.Red;
+                    break;
+                default:
+                    mark.Fill = Brushes.Gray;
+                    break;
+            }
+
+            mark.RenderTransformOrigin = new Point(0.5, 0.5);
+
+            return mark;
         }
 
     }
